8f59321dca2792bd934b70ac4d2bce77f6184e43
ko-tisha <ko.tisha@list.ru>
2025-01-29 12:02:29 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+
+// 1 Задача
+
+const str = "abcde"
+console.log (str[0], str[1], str[4]);
+
+// 2 Задача
+
+let a = parseInt (prompt ("Введите первое число:"));
+let b = parseInt (prompt ("Введите второе число:"));
+console.log (a > 0 || b > 0);
+
+// 3 Задача
+
+let age = parseInt (prompt("Введите возраст пользователя:"));
+if (age >= 18) {
+    console.log ("Совершеннолетний");
+} else {
+    console.log ("Несовершеннолетний");
+}
